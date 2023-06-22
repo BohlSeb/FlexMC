72bd88076d961f694d30631b82ed1eca3a5a9a43
itsguccy <bohlensebastian@aol.de>
2023-06-22 22:11:05 +0200

    remove unwanted file (reintroduce later)

--- flexmc/flexmc/shuntingyard.cpp
+++
@@ -1,7 +0,0 @@
-#include "shuntingyard.h"
-
-//std::ostream& flexMC::operator<<(std::ostream& output, const PfixLine& line) {
-//    for (auto val : line.stack) {
-//        output << val.data << " ";
-//    }
-//}
