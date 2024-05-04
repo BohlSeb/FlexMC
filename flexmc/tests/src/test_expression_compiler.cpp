612df9cbef59528a0b1747a8f2fd87b12453879a
Sebastian Bohlen <bohlensebastian@aol.de>
2024-05-04 18:40:01 +0200

    test vector calculations

--- flexmc/tests/src/test_expression_compiler.cpp
+++ flexmc/tests/src/test_expression_compiler.cpp
@@ -30,7 +30,7 @@ public:
 			{"2 + 3", 5.0},
 			{"4 * 5 - 7", 13.0},
 			{"8 + 9 * 10", 98.0},
-			{"3 * (5 + 2)", 21.0},
+			{"EXP(0.0) * 3 * (5 + 2)", 21.0},
 			{"(4 + 6) * 8", 80.0},
 			{"2 * (3 + 4)", 14.0},
 			{"6 * 7 + 8", 50.0},
@@ -39,9 +39,9 @@ public:
 			{"((2 + 3) * 4 - 5) / 6", 2.5},
 			{"3 + 4 * 5", 23.0},
 			{"6 * 7 + 8", 50.0},
-			{"2**3 + 4**5", 1032},
+			{"2**3 + SQRT(16) **5", 1032},
 			{"5**(2+3)", 3125.0},
-			{"3 + 4**2 * 5", 83.0},
+			{"3 + SQUARE(2)**2 * 5", 83.0},
 			{"2 * 3**4 - 5", 157},
 			{"(3 + 4)**2", 49.0},
 			{"5**(2 + 3) / 6", 520.8333333333334},
@@ -57,9 +57,9 @@ public:
 			{"10 * -((5 - 2)**2 - 4) / 3", -16.666666666666668},
 			{"2**-3 - -5 * (4 - 2) + 8 / 4", 12.125},
 			{"-(7 - 3)**2 / (5 - 2) + 4 * (9 - 2)", 22.666666666666668},
-			{"2 + -(5 * 4) / (3 - 1)**2", -3.0},
+			{"2 + -ABS(-(5 * 4)) / (3 - 1)**2", -3.0},
 			{"(6 * 3) / (5 - 2)**-2 + -(8 - 2) * 3", 144.0},
-			{"(2 * +3**-2) / -(5 - 2) + +10", 9.925925925925926},
+			{"(2 * +3**LOG(EXP(-2))) / -(5 - 2) + +10", 9.925925925925926},
 			{"(8**2) / -((5 - 2) * (3 + 1)) - 7 + 2", -10.333333333333332},
 			{"((9 - 3) * (4 + 7) / 2)**2 + -10", 1079.0},
 			{"(12 / 3) * -(7 - (4 + 1))**2 + 10 / 2", -11.0},
@@ -76,8 +76,8 @@ public:
 			std::vector<std::shared_ptr<PostFixItem>> main_l = compiler.compiled();
 			CalcStacks c_stacks;
 
-			for (auto iter = main_l.cbegin(); iter != main_l.cend(); ++iter) {
-				(*iter)->evaluate(c_stacks);
+			for (auto it = main_l.cbegin(); it != main_l.cend(); ++it) {
+				(*it)->evaluate(c_stacks);
 			}
 			double result = c_stacks.scalarsBack();
 			c_stacks.popScalar();
@@ -87,8 +87,8 @@ public:
 			int trys = 10000;
 			result = 0.0;
 			for (int i = 0; i < trys; ++i) {
-				for (auto iter = main_l.cbegin(); iter != main_l.cend(); ++iter) {
-					(*iter)->evaluate(c_stacks);
+				for (auto it = main_l.cbegin(); it != main_l.cend(); ++it) {
+					(*it)->evaluate(c_stacks);
 				}
 				result += c_stacks.scalarsBack();
 				c_stacks.popScalar();
@@ -96,6 +96,71 @@ public:
 			}
 			double average = result / trys;
 			Assert::AreEqual(c.result, average, 1e-10);
+		}
+
+	}
+
+	void areEqualVector(const std::vector<double>& expected, const std::vector<double>& result, const double& precision) {
+		auto res = result.cbegin();
+		for (auto it = expected.cbegin(); it != expected.cend(); ++it) {
+			Assert::AreEqual(*it, *res, precision);
+			++res;
+		}
+	}
+
+	TEST_METHOD(TestCalcOperatorsVector) {
+		using vec = std::vector<double>;
+		struct TestCase {
+			std::string infix;
+			const vec result;
+		};
+
+		TestCase TestData[] = {
+			{"[2, 2] + [3, 2]", vec({5.0, 4.0})},
+			{"[2, 2] / [2, 2]", vec({1.0, 1.0})},
+			{"[2, 2] * [2, 2]", vec({4.0, 4.0})},
+			{"[-2, -2] + [2, 2]", vec({0.0, 0.0})},
+			{"[-2, -2] - [2, 2]", vec({-4.0, -4.0})},
+			{"EXP(LOG([2, 2]))", vec({2.0, 2.0})},
+			{"ABS(-1 * [4, 4, 4, 4, 4])", vec({4, 4, 4, 4, 4})},
+			{"SQUARE([-1, 1, 2, 2])", vec({1, 1, 4, 4})},
+			{"-SQRT([4, 4, 4, 4])", vec({-2, -2, -2, -2})},
+			{"ABS(-1 * [4, 4, 4, 4, 4])", vec({4, 4, 4, 4, 4})},
+			{"[2, 1] * (3 + 4)", vec({14.0, 7.0})},
+			{"2**[3, 3] + 4**5", vec({1032, 1032})},
+			{"3 + 4 * 5 / (EXP(LOG([2, 2] - [1, 1])))", vec({23.0, 23.0})},
+		};
+
+		for (auto& c : TestData) {
+
+			Lexer lexer = Lexer(c.infix);
+			ExpressionParser parser = ExpressionParser(lexer);
+			std::deque<Token> parsed = parser.parseLine();
+			ExpressionCompiler compiler = ExpressionCompiler(parsed);
+
+			std::vector<std::shared_ptr<PostFixItem>> main_l = compiler.compiled();
+			CalcStacks c_stacks;
+
+			for (auto it = main_l.cbegin(); it != main_l.cend(); ++it) {
+				(*it)->evaluate(c_stacks);
+			}
+			vec result = c_stacks.vectorsBack();
+			c_stacks.popVector();
+			Assert::IsTrue(c_stacks.ready());
+			TestExpressionCompiler::areEqualVector(c.result, result, 1e-14);
+
+			int trys = 10000;
+			vec result_2 = vec(result.size(), 0.0);
+			for (int i = 0; i < trys; ++i) {
+				for (auto it = main_l.cbegin(); it != main_l.cend(); ++it) {
+					(*it)->evaluate(c_stacks);
+				}
+				std::transform(result_2.cbegin(), result_2.cend(), c.result.cbegin(), result_2.begin(), std::plus<double>());
+				c_stacks.popVector();
+				Assert::IsTrue(c_stacks.ready());
+			}
+			std::transform(result_2.cbegin(), result_2.cend(), result_2.begin(), [=](double v) {return v / trys; });
+			TestExpressionCompiler::areEqualVector(c.result, result_2, 1e-10);
 		}
 
 	}
