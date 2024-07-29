fbbcbdc393ec9d7dbbb6db627cc4627bc3682deb
Sebastian Bohlen <bohlensebastian@aol.de>
2024-07-29 23:44:50 +0200

    Revert "static variable storage access restricted to a friend compiler class"
    This reverts commit 4c84db18aa980361c2fa375e9cd639d2d387a735.
    
    accidentally pushed to master

--- flexmc/src/utils.h
+++ flexmc/src/utils.h
@@ -3,11 +3,6 @@
 #include <string_view>
 #include <unordered_map>
 
-// suggested by SonarLint cpp:S6045 to use for string keys in std::unordered_map
-// "Transparent function objects should be used with associative "std::string" containers"
-// https://rules.sonarsource.com/cpp/tag/since-c++14/RSPEC-6045/
-
-
 namespace flexMC
 {
 
@@ -15,7 +10,7 @@ namespace flexMC
     {
         using is_transparent [[maybe_unused]] = void;
 
-        std::size_t operator()(const std::string_view s_view) const
+        inline std::size_t operator()(const std::string_view s_view) const
         { return std::hash<std::string_view>{}(s_view); }
     };
 
